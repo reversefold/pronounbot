1572b6cd484ad28e99e85ea1ca32537bc57a6004
vsams14 <vsams14@gmail.com>
2018-12-30 15:34:19 +0200

    Management mode to prevent Heroku response

--- bot.js
+++ bot.js
@@ -8,16 +8,32 @@ client.on("ready", () => {
  
 client.on("message", (message) => {
 	if(message.content.substring(0,1) == config.prefix && !message.author.bot){
-		var cmd = message.content.split(" ");
+		var cmd = message.content.toLowerCase().split(/ +/g);
 		var args = cmd.slice(1, cmd.length);
 		cmd = cmd[0];
 		if(cmd == "/pronoun"){
+			message.delete();
 			switch(args[0]) {
-				case "check":
-					message.channel.send("Checking roles");
+				/*case "check":
+					if(args[1]){
+						var myRole = message.guild.roles.find(role => role.name.toLowerCase() === args[1]);
+						if(myRole){
+							message.channel.send('You have the role "'+myRole.name+'"!');
+						}else{
+							message.channel.send('You do not have the role "'+args[1]+'"!');
+						}
+					}else{
+						message.channel.send("Please specify a role to check!");
+					}
+					break;*/
+				case "set":
+					var roles = message.guild.roles;
+					for (var k in roles) {
+						message.channel.send(k.name);
+					}
 					break;
 				default:
-					message.channel.send('The command "' + args[0] + ' {...}" is not supported yet.');
+					//message.channel.send('The command "' + cmd + " " + args[0] + ' {...}" is not supported yet.');
 					break;
 			}
 		}
